ef994f11f744e7bd52fbc3cc4b1d729ee9d64a37
ahmmed essam <ahmmedisam2@gmail.com>
2021-03-23 02:38:04 +0200

    Tasks
    Arithmetic operators
    IF Condition

---
+++ Arithmetic operators/bin/main2.dart
@@ -0,0 +1,17 @@
+void main () {
+  /* task
+   * AHMED ESSAM
+   * "Arithmetic operators"
+   */
+  int a = 55;
+  int b = 10;
+  int w = a + b;
+  print (w);
+  int x = a - b;
+  print (x);
+  int y = a * b;
+  print (y);
+  double z = a / b; // used "double" Because the result is a decimal number '5.5'
+  print (z);
+
+}
---
+++ Arithmetic operators/test/Task_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:Task_2/Task_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
